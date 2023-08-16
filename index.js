073cfc4ce25aafa34cbe76ade44f219d909fa9bd
NICK MUGO NJURU <nicknjuru13@gmail.com>
2023-08-16 10:01:33 +0300

    Completed asignment

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
-}
-
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+  function fetchBooks() {
+    return fetch('https://anapioficeandfire.com/api/books')
+      .then(response => response.json())
+      .then(data =>{renderBooks(data)});
+  }
+  function renderBooks(books){
+    let main =document.querySelector("main");
+    books.forEach(element => {
+      let li=document.createElement("li");
+      li.innerHTML=element.name;
+      main.appendChild(li);
+    });
+  }
+      
